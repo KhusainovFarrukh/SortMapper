b0006f5bcb4dc4d78c74268c0918b5b188ae9ef9
f_khusainov <farrukhbekkhusainov@gmail.com>
2024-10-31 22:04:24 +0500

    J-E: Removed scanBasePackages after KT-L changes

--- sort-mapper-example-java/src/main/java/kh/farrukh/sortmapperexamplejava/SortMapperExampleJavaApplication.java
+++ sort-mapper-example-java/src/main/java/kh/farrukh/sortmapperexamplejava/SortMapperExampleJavaApplication.java
@@ -3,9 +3,8 @@ package kh.farrukh.sortmapperexamplejava;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(
-    scanBasePackages = {"kh.farrukh.sortmapperexamplejava", "kh.farrukh.sortmapper"}
-)public class SortMapperExampleJavaApplication {
+@SpringBootApplication
+public class SortMapperExampleJavaApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(SortMapperExampleJavaApplication.class, args);
